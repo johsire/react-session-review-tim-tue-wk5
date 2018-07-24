8f23b3403697ee600b5e84f2032362375fbf439e
Josiah Maina <josiah5115@gmail.com>
2018-07-24 17:53:22 -0600

    set up session

--- server/server.js
+++ server/server.js
@@ -6,7 +6,23 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(session({
+  secret: 'scoobysnack',
+  saveUnitinitialized: false,
+  resave: false
+}))
+
 //MiddleWare
+
+app.post('/api.login', (req, res) => {
+  const {role} = req.body;
+  res.stateus(200).send(role);
+  req.session.user = {
+    id: req.session.id,
+    role: role
+  };
+  res.status(200).send(role);
+});
 
 const port = 3010;
 app.listen(port, () => console.log(`Magic Happens on Port:${port}`));
