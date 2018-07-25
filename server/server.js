6202dc0202ff037d2aed123bc33d8e03372aadfc
Josiah Maina <josiah5115@gmail.com>
2018-07-24 18:26:24 -0600

    more changes

--- server/server.js
+++ server/server.js
@@ -16,13 +16,22 @@ app.use(session({
 
 app.post('/api.login', (req, res) => {
   const {role} = req.body;
-  res.stateus(200).send(role);
+  res.status(200).send(role);
   req.session.user = {
     id: req.session.id,
     role: role
   };
   res.status(200).send(role);
 });
+
+
+app.get('/api/permission', (req, res) => {
+  if(req.session.user.role === 'admin') {
+    res.status(200).send('onwards')
+  } else {
+    res.status(200).send(null);
+  }
+})
 
 const port = 3010;
 app.listen(port, () => console.log(`Magic Happens on Port:${port}`));
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,12 +2,25 @@ import React from "react";
 import axios from "axios";
 
 export default class Dashboard extends React.Component {
+
+  handlePermissions() {
+    axios.get('/api/permission').then(response => {
+      if(response.data) {
+        this.props.history.push("/admin");
+        console.log('he is good')
+      } else {
+        console.log('nah');
+      }
+    })
+  }
+
+
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
-        <button>Go to Admin Page (Only Admins)</button>
-        <button>Go to Student Page</button>
+        <button onClick={() => this.handlePermissions()}>Go to Admin Page (Only Admins)</button>
+        <button onClick={() => this.handStudentPage()}>Go to Student Page</button>
       </div>
     );
   }
